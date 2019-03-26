474bba644765a38bbbcce70b7246b7aba28bd9a4
imrod22 <imrod.nameless@gmail.com>
2019-03-26 12:48:07 -0300

    Testing propose
    First commit.

--- HomeSwitchHome/src/app/app.module.ts
+++ HomeSwitchHome/src/app/app.module.ts
@@ -4,16 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AngularFontAwesomeModule } from 'angular-font-awesome'
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AngularFontAwesomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
