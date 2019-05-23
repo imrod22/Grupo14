ff758b882970b7ddf81b596ffaed0e89deffdbad
tadeovelis <48958467+tadeovelis@users.noreply.github.com>
2019-05-23 01:00:00 -0300

    Add files via upload

---
+++ bajaSubasta.js
@@ -0,0 +1,14 @@
+﻿{
+    // dentro del controller de subasta
+
+    if (!($scope.fechaComienzo <= Date.now)) {
+        $http.post("/Home/EliminarSubasta", {
+            'idPropiedad': $scope.idPropiedad
+        }).then(function (response) {
+            console.log("Se ha eliminado la subasta con éxito.")
+        }.error(
+        )
+        ))
+    }
+
+}
