0c880896b5200236fc076b9299b7ad788d236098
tadeovelis <48958467+tadeovelis@users.noreply.github.com>
2019-05-23 00:31:37 -0300

    Add files via upload

---
+++ Home (modificarResidencia).js
@@ -0,0 +1,27 @@
+﻿{
+    // adentro del controller de propiedades
+
+    function corroborarInputs() {
+        // no verifica que no exista una con el mismo nombre
+        return (
+            ($scope.nombre == '' || $scope.nombre.size < 8)
+            || ($scope.descripcion.size < 20)
+            || ($scope.ubicacion == '')
+        )
+    }
+
+    $scope.aceptar() {
+        if (!corroborarInputs()) {
+            $http.post("/Home/ModificarPropiedad", {
+                'nombre': $scope.nombre,
+                'domicilio': $scope.domicilio,
+                'descripcion': $scope.descripcion,
+                'pais': $scope.pais
+            }).then(function (response) {
+                console.log("Se ha creado la residencia con éxito.")
+            }.error(
+                alert("No se puede agregar la propiedad. Ya existe.")
+            )
+        }
+    }
+}
