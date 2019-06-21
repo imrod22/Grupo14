af679dc10c33598db27f359ead3faf0fadb63c4e
Tax <tadeovelis@gmail.com>
2019-06-21 00:22:33 -0300

    Update RegistroSection.js

--- HomeSwitchHome/Scripts/app/RegistroSection.js
+++ HomeSwitchHome/Scripts/app/RegistroSection.js
@@ -3,19 +3,25 @@
 
         var cliente = $('#registro-form').serialize();
 
-        $.ajax({
-            type: "POST",
-            url: "/Home/RegistrarUsuario",
-            data: cliente,
-            success: function (response) {
+        if ($('#Password').val().length < 8) {
+            console.log($('#Password').val().length);
+            swal("Home Switch Home", "La contraseña tiene que ser mayor o igual a 8 caracteres", "error");
+        }
+        else {
+            $.ajax({
+                type: "POST",
+                url: "/Home/RegistrarUsuario",
+                data: cliente,
+                success: function (response) {
                     swal("Home Switch Home", "Su solicitud de registro ha sido registrada. Se le notificará cuando pueda acceder.", "success");
                     window.location.href = response;
-                            
-            },
-            error: function (jqXHR) {
-                swal("Home Switch Home", jqXHR.responseJSON, "error");
-                
-            }
-        });
+
+                },
+                error: function (jqXHR) {
+                    swal("Home Switch Home", jqXHR.responseJSON, "error");
+
+                }
+            });
+        }
     });
 })
